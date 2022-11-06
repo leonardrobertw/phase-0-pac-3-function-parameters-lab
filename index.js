95f2ba93b2b43201af4677180f1ea27dae4c1d00
Robert <leonardrobertw@gmail.com>
2022-11-05 20:13:24 -0400

    commit tree d2a84f4229fa1288826e9eb53976eaf1dd1464d6

---
+++ index.js
@@ -0,0 +1,8 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+      }
+  console.log(introduction("Aki"))
+  function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+  }
+   introductionWithLanguage("Aki", "Ember.js"
